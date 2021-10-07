6be1ea4582cfbbad3ec584911f7c75da1b9539bb
Joe Zucco <josephzucco@gmail.com>
2021-10-07 13:52:42 -0400

    create and scope button and count components

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = (props) => {
+  return (
+    <div>
+      <h1>Button component</h1>
+    </div>
+  );
+};
---
+++ src/components/Count.js
@@ -0,0 +1,9 @@
+export const Count = () => {
+  return (
+    <div>
+      <h1>Count component</h1>
+    </div>
+  );
+};
+
+export default Count;
