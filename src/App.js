0ba9e528e978c28df97a228c7fbd79200163c905
Joe Zucco <josephzucco@gmail.com>
2021-10-07 13:49:51 -0400

    set state, pass onClick functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //Create State - a variable that, when changed, updates a component.
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className='App'>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Add</button>
+      <button onClick={() => setCounter(counter - 1)}>Subtract</button>
     </div>
   );
 }
